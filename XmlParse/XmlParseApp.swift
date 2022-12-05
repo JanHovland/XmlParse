88202c61b9ad90e4f2d57126d31d5d0e0299ffeb
Jan Hovland <jan.hovland@lyse.net>
2022-12-05 09:31:21 +0100

    Initial Commit

---
+++ XmlParse/XmlParseApp.swift
@@ -0,0 +1,17 @@
+//
+//  XmlParseApp.swift
+//  XmlParse
+//
+//  Created by Jan Hovland on 05/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct XmlParseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
